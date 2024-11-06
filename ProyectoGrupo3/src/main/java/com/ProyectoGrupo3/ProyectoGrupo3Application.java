387f47f28ff4abdffcda52836261b5ea23d510ac
AllanAzo17 <aazofeifa60857@ufide.ac.cr>
2024-11-05 18:08:29 -0600

    Primer commit

---
+++ ProyectoGrupo3/src/main/java/com/ProyectoGrupo3/ProyectoGrupo3Application.java
@@ -0,0 +1,13 @@
+package com.ProyectoGrupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGrupo3Application.class, args);
+	}
+
+}
---
+++ ProyectoGrupo3/src/test/java/com/ProyectoGrupo3/ProyectoGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoGrupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
